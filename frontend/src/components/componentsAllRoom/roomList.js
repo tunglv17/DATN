ff5175be3f1f88d4fbd7b4324abff097ed2267b3
tunglvph <tungrich2001@gmail.com>
2021-10-31 18:37:14 +0700

    update

---
+++ frontend/src/components/componentsAllRoom/roomList.js
@@ -0,0 +1,229 @@
+import React from 'react'
+
+const RoomList = () => {
+ return (
+  <div>
+   <section className="ftco-section bg-light">
+    <div className="container">
+     <div className="row">
+      <div className="col-lg-9">
+       <div className="row">
+        <div className="col-sm col-md-6 col-lg-4 ftco-animate">
+         <div className="room">
+          <a href="rooms-single.html" className="img d-flex justify-content-center align-items-center" style={{ backgroundImage: 'url(images/room-6.jpg)' }}>
+           <div className="icon d-flex justify-content-center align-items-center">
+            <span className="icon-search2" />
+           </div>
+          </a>
+          <div className="text p-3 text-center">
+           <h3 className="mb-3"><a href="rooms-single.html">Luxury Room</a></h3>
+           <p><span className="price mr-2">$500.00</span> <span className="per">Một đêm</span></p>
+           <ul className="list">
+            <li><span>Max:</span> 5 Người</li>
+            <li><span>Size:</span> 45 m2</li>
+            <li><span>View:</span> Hướng Biển</li>
+            <li><span>Giường:</span> 2</li>
+           </ul>
+           <hr />
+           <p className="pt-1"><a href="room-single.html" className="btn-custom">Book Now <span className="icon-long-arrow-right" /></a></p>
+          </div>
+         </div>
+        </div>
+        <div className="col-sm col-md-6 col-lg-4 ftco-animate">
+         <div className="room">
+          <a href="rooms-single.html" className="img d-flex justify-content-center align-items-center" style={{ backgroundImage: 'url(images/room-6.jpg)' }}>
+           <div className="icon d-flex justify-content-center align-items-center">
+            <span className="icon-search2" />
+           </div>
+          </a>
+          <div className="text p-3 text-center">
+           <h3 className="mb-3"><a href="rooms-single.html">Luxury Room</a></h3>
+           <p><span className="price mr-2">$500.00</span> <span className="per">Một đêm</span></p>
+           <ul className="list">
+            <li><span>Max:</span> 5 Người</li>
+            <li><span>Size:</span> 45 m2</li>
+            <li><span>View:</span> Hướng Biển</li>
+            <li><span>Giường:</span> 2</li>
+           </ul>
+           <hr />
+           <p className="pt-1"><a href="room-single.html" className="btn-custom">Book Now <span className="icon-long-arrow-right" /></a></p>
+          </div>
+         </div>
+        </div>
+        <div className="col-sm col-md-6 col-lg-4 ftco-animate">
+         <div className="room">
+          <a href="rooms-single.html" className="img d-flex justify-content-center align-items-center" style={{ backgroundImage: 'url(images/room-6.jpg)' }}>
+           <div className="icon d-flex justify-content-center align-items-center">
+            <span className="icon-search2" />
+           </div>
+          </a>
+          <div className="text p-3 text-center">
+           <h3 className="mb-3"><a href="rooms-single.html">Luxury Room</a></h3>
+           <p><span className="price mr-2">$500.00</span> <span className="per">Một đêm</span></p>
+           <ul className="list">
+            <li><span>Max:</span> 5 Người</li>
+            <li><span>Size:</span> 45 m2</li>
+            <li><span>View:</span> Hướng Biển</li>
+            <li><span>Giường:</span> 2</li>
+           </ul>
+           <hr />
+           <p className="pt-1"><a href="room-single.html" className="btn-custom">Book Now <span className="icon-long-arrow-right" /></a></p>
+          </div>
+         </div>
+        </div>
+        <div className="col-sm col-md-6 col-lg-4 ftco-animate">
+         <div className="room">
+          <a href="rooms-single.html" className="img d-flex justify-content-center align-items-center" style={{ backgroundImage: 'url(images/room-6.jpg)' }}>
+           <div className="icon d-flex justify-content-center align-items-center">
+            <span className="icon-search2" />
+           </div>
+          </a>
+          <div className="text p-3 text-center">
+           <h3 className="mb-3"><a href="rooms-single.html">Luxury Room</a></h3>
+           <p><span className="price mr-2">$500.00</span> <span className="per">Một đêm</span></p>
+           <ul className="list">
+            <li><span>Max:</span> 5 Người</li>
+            <li><span>Size:</span> 45 m2</li>
+            <li><span>View:</span> Hướng Biển</li>
+            <li><span>Giường:</span> 2</li>
+           </ul>
+           <hr />
+           <p className="pt-1"><a href="room-single.html" className="btn-custom">Book Now <span className="icon-long-arrow-right" /></a></p>
+          </div>
+         </div>
+        </div>
+        <div className="col-sm col-md-6 col-lg-4 ftco-animate">
+         <div className="room">
+          <a href="rooms-single.html" className="img d-flex justify-content-center align-items-center" style={{ backgroundImage: 'url(images/room-6.jpg)' }}>
+           <div className="icon d-flex justify-content-center align-items-center">
+            <span className="icon-search2" />
+           </div>
+          </a>
+          <div className="text p-3 text-center">
+           <h3 className="mb-3"><a href="rooms-single.html">Luxury Room</a></h3>
+           <p><span className="price mr-2">$500.00</span> <span className="per">Một đêm</span></p>
+           <ul className="list">
+            <li><span>Max:</span> 5 Người</li>
+            <li><span>Size:</span> 45 m2</li>
+            <li><span>View:</span> Hướng Biển</li>
+            <li><span>Giường:</span> 2</li>
+           </ul>
+           <hr />
+           <p className="pt-1"><a href="room-single.html" className="btn-custom">Book Now <span className="icon-long-arrow-right" /></a></p>
+          </div>
+         </div>
+        </div>
+        <div className="col-sm col-md-6 col-lg-4 ftco-animate">
+         <div className="room">
+          <a href="rooms-single.html" className="img d-flex justify-content-center align-items-center" style={{ backgroundImage: 'url(images/room-6.jpg)' }}>
+           <div className="icon d-flex justify-content-center align-items-center">
+            <span className="icon-search2" />
+           </div>
+          </a>
+          <div className="text p-3 text-center">
+           <h3 className="mb-3"><a href="rooms-single.html">Luxury Room</a></h3>
+           <p><span className="price mr-2">$500.00</span> <span className="per">Một đêm</span></p>
+           <ul className="list">
+            <li><span>Max:</span> 5 Người</li>
+            <li><span>Size:</span> 45 m2</li>
+            <li><span>View:</span> Hướng Biển</li>
+            <li><span>Giường:</span> 2</li>
+           </ul>
+           <hr />
+           <p className="pt-1"><a href="room-single.html" className="btn-custom">Book Now <span className="icon-long-arrow-right" /></a></p>
+          </div>
+         </div>
+        </div>
+       </div>
+      </div>
+      <div className="col-lg-3 sidebar">
+       <div className="sidebar-wrap bg-light ftco-animate">
+        <h3 className="heading mb-4">Tìm kiếm nâng cao</h3>
+        <form action="#">
+         <div className="fields">
+          <div className="form-group">
+           <input type="text" id="checkin_date" className="form-control checkin_date" placeholder="Ngày Đến" />
+          </div>
+          <div className="form-group">
+           <input type="text" id="checkin_date" className="form-control checkout_date" placeholder="Ngày Trả" />
+          </div>
+          <div className="form-group">
+           <div className="select-wrap one-third">
+            <div className="icon"><span className="ion-ios-arrow-down" /></div>
+            <select name id className="form-control">
+             <option value>Loại Phòng</option>
+             <option value>Thượng Hạng</option>
+             <option value>Trung Bình</option>
+            </select>
+           </div>
+          </div>
+          <div className="form-group">
+           <div className="select-wrap one-third">
+            <div className="icon"><span className="ion-ios-arrow-down" /></div>
+            <select name id className="form-control">
+             <option value>1 Người</option>
+             <option value>2 Người</option>
+            </select>
+           </div>
+          </div>
+          <div className="form-group">
+           <div className="range-slider">
+            <span>
+             <input type="number" defaultValue={25000} min={0} max={120000} />	-
+             <input type="number" defaultValue={50000} min={0} max={120000} />
+            </span>
+            <input defaultValue={1000} min={0} max={120000} step={500} type="range" />
+            <input defaultValue={50000} min={0} max={120000} step={500} type="range" />
+           </div>
+          </div>
+          <div className="form-group">
+           <input type="submit" defaultValue="Search" className="btn btn-primary py-3 px-5" />
+          </div>
+         </div>
+        </form>
+       </div>
+       <div className="sidebar-wrap bg-light ftco-animate">
+        <h3 className="heading mb-4">Đánh Giá Sao</h3>
+        <form method="post" className="star-rating">
+         <div className="form-check">
+          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+          <label className="form-check-label" htmlFor="exampleCheck1">
+           <p className="rate"><span><i className="icon-star" /><i className="icon-star" /><i className="icon-star" /><i className="icon-star" /><i className="icon-star" /></span></p>
+          </label>
+         </div>
+         <div className="form-check">
+          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+          <label className="form-check-label" htmlFor="exampleCheck1">
+           <p className="rate"><span><i className="icon-star" /><i className="icon-star" /><i className="icon-star" /><i className="icon-star" /><i className="icon-star-o" /></span></p>
+          </label>
+         </div>
+         <div className="form-check">
+          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+          <label className="form-check-label" htmlFor="exampleCheck1">
+           <p className="rate"><span><i className="icon-star" /><i className="icon-star" /><i className="icon-star" /><i className="icon-star-o" /><i className="icon-star-o" /></span></p>
+          </label>
+         </div>
+         <div className="form-check">
+          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+          <label className="form-check-label" htmlFor="exampleCheck1">
+           <p className="rate"><span><i className="icon-star" /><i className="icon-star" /><i className="icon-star-o" /><i className="icon-star-o" /><i className="icon-star-o" /></span></p>
+          </label>
+         </div>
+         <div className="form-check">
+          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+          <label className="form-check-label" htmlFor="exampleCheck1">
+           <p className="rate"><span><i className="icon-star" /><i className="icon-star-o" /><i className="icon-star-o" /><i className="icon-star-o" /><i className="icon-star-o" /></span></p>
+          </label>
+         </div>
+        </form>
+       </div>
+      </div>
+     </div>
+    </div>
+   </section>
+
+  </div>
+ )
+}
+
+export default RoomList
